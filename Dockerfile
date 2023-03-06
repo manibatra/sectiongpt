8aeb60b335d77c66c2278c1d894aa74daf992c05
Mani Batra <manibatra2002@gmail.com>
2023-03-06 22:27:01 +1100

    update gradio port

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,5 @@ COPY . .
 RUN pip install -r requirements.txt
 
 CMD ["python", "app.py"]
+
+EXPOSE 7860
--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def question_answer(question):
 
 
 gr.Interface(fn=question_answer, inputs=["text"], outputs=["markdown"], title="SectionGPT",
-             description="Instant answers to Section docs").launch()
+             description="Instant answers to Section docs", server_port=80).launch()
