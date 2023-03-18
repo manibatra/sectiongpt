d1ab3800787ca921bc0b1e808fcc4438e062f1b3
Mani Batra <manibatra2002@gmail.com>
2023-03-18 18:53:36 +1100

    Recaclulate the data and the embeddings

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from embeddings import *
 from completions import *
 
 DOCUMENTS_PATH = "/Users/manibatra/code/section/docs/docs"
-CSV_PATH = "./data/data.csv"
-EMBEDDINGS_CSV_PATH = "./data/embeddings.csv"
+CSV_PATH = "./data/data-concise.csv"
+EMBEDDINGS_CSV_PATH = "./data/embeddings-concise.csv"
 
 if __name__ == '__main__':
     # Organise the data into a CSV file
@@ -13,19 +13,33 @@ if __name__ == '__main__':
     # Compute the document embeddings
     df = read_csv(CSV_PATH, header=0)
     df = df.set_index(['title', 'description'])
+    df = df.sort_index()
+
+    # Test
     # print(f"{len(df)} rows in the data")
-    # document_embeddings = compute_doc_embeddings(df)
-    # save_embeddings(document_embeddings, EMBEDDINGS_CSV_PATH)
+    # doc_index = ('Kubernetes and Section ', 'Kubernetes API and Section')
+    # print(df.loc[doc_index].content.values)
+    #
+    # # Create and save the embeddings
+    document_embeddings = compute_doc_embeddings(df)
+    save_embeddings(document_embeddings, EMBEDDINGS_CSV_PATH)
 
-    # Load the document embeddings
-    document_embeddings = load_embeddings(EMBEDDINGS_CSV_PATH)
-
-    # example_entry = list(document_embeddings.items())[0]
-    # print(f"{example_entry[0]} : {example_entry[1][:5]}... ({len(example_entry[1])} entries)")
-
-    # Read the query from the user input
-    query = input("Enter your query: ")
-    potential_contexts = potential_contexts_by_query_similarity(query, df, document_embeddings)
-
-    # Get the completion for a query
-    print(create_completion(query, potential_contexts))
+    # # Load the document embeddings
+    # document_embeddings = load_embeddings(EMBEDDINGS_CSV_PATH)
+    #
+    # # Test
+    # # example_entry = list(document_embeddings.items())[0]
+    # # print(f"{example_entry[0]} : {example_entry[1][:5]}... ({len(example_entry[1])} entries)")
+    #
+    # # Read the query from the user input
+    # query = input("Enter your query: ")
+    # potential_contexts = potential_contexts_by_query_similarity(query, df, document_embeddings)
+    # # print('Sending to GPT:' + ''.join(potential_contexts[:5]))
+    #
+    # # Get the completion for a query
+    # completion_generator = create_completion(query, potential_contexts)
+    # for chunk in completion_generator:
+    #     chunk_message = chunk['choices'][0]['delta']
+    #     if 'content' in chunk_message:
+    #         message = chunk_message['content']
+    #         print(message, end='')
