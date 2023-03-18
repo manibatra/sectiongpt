514ffd9625caeadf658155f3ffd181e52cc244b1
Mani Batra <manibatra2002@gmail.com>
2023-03-18 18:53:00 +1100

    Enable streaming and improve the prompt

--- completions.py
+++ completions.py
@@ -1,18 +1,11 @@
-import numpy as np
 import openai
 import tiktoken
-
-from embeddings import order_document_sections_by_query_similarity
 
 MAX_TOKEN_LENGTH = 4096
 MODEL = "gpt-4"
 ENCODING_FOR_MODEL = "gpt-3.5-turbo-0301"
 SYSTEM_SETUP_MESSAGE = 'You are a helpful assistant that works for Section and answers question based on the ' \
-                       'provided context. Be concise, format the answer in lists/steps where possible, supply code ' \
-                       'examples where ' \
-                       'possible  and if you\'re unsure of the ' \
-                       'answer, say "Sorry, I don\'t ' \
-                       'know."'
+                       'provided context.'
 
 
 def construct_messages(query: str, contexts: list[str]) -> list[dict[str, str]]:
@@ -32,11 +25,20 @@ def construct_messages(query: str, contexts: list[str]) -> list[dict[str, str]]:
 
     # add the context but add it after finalising the context
     num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>
-    num_tokens += len(encoding.encode('Context: '))
+    context_setup_prompt = 'Be thorough. If providing relative links (starting with /) at all such as /img/xyz then prefix ' \
+                           'them with "https://www.section.io/docs" to https://www.section.io/docs/img/xyz make them  but don\'t change anything else. Provide the answer again even if it is in the context. ' \
+                           'Format the answer in lists/steps where possible, supply code examples where possible  and ' \
+                           'if you\'re unsure of the answer, say "Sorry, I don\'t know."\n'
+    context_suffix = 'Context: '
+    num_tokens += len(encoding.encode(context_setup_prompt))
+    num_tokens += len(encoding.encode(context_suffix))
 
     # send at least 1 context
     final_context = contexts.pop(0)
-    num_tokens += len(encoding.encode(final_context))
+    try:
+        num_tokens += len(encoding.encode(final_context))
+    except Exception as e:
+        print(final_context)
 
     # for shorter context send multiple up to MAX_TOKEN_LENGTH
     for context in contexts:
@@ -47,13 +49,14 @@ def construct_messages(query: str, contexts: list[str]) -> list[dict[str, str]]:
             final_context += '\n' + context
             num_tokens = potential_num_tokens
 
-    messages.append({"role": "user", "content": final_context + formatted_query})
+    messages.append(
+        {"role": "user", "content": context_setup_prompt + context_suffix + final_context + formatted_query})
 
     # print(messages)
     return messages
 
 
-def create_completion(query: str, contexts: list[str]) -> str:
+def create_completion(query: str, contexts: list[str]):
     """
     Create a completion given a query and a context.
     """
@@ -64,7 +67,7 @@ def create_completion(query: str, contexts: list[str]) -> str:
     response = openai.ChatCompletion.create(
         model=MODEL,
         messages=messages,
-        temperature=0
+        temperature=0,
+        stream=True,
     )
-
-    return response['choices'][0]['message']['content']
+    return response
